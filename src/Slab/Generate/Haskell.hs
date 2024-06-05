93b58ef6be2f9e37125308bf9b11e8cde25508f1
Vo Minh Thu <thu@hypered.io>
2024-06-06 00:27:52 +0200

    Use a proper pretty-printing library.
    The example used in a previous commit message now looks like
    
        H.div ! A.class_ (H.toValue "a") $
          H.div ! A.class_ (H.toValue "b") $
            mempty
    
    When multiple attributes are given, they are aligned on the `!`. And
    when multiple children are given, the end of the last attribute is `$
    do` instead of just `$`.

--- src/Slab/Generate/Haskell.hs
+++ src/Slab/Generate/Haskell.hs
@@ -2,11 +2,11 @@ module Slab.Generate.Haskell
   ( renderHs
   ) where
 
-import Data.List (intersperse)
 import Data.Text (Text)
 import Data.Text qualified as T
-import Data.Text.Lazy qualified as TL
-import Data.Text.Lazy.IO qualified as TL
+import Data.Text.IO qualified as T
+import Prettyprinter
+import Prettyprinter.Render.Text
 import Slab.Evaluate qualified as Evaluate
 import Slab.Syntax qualified as Syntax
 
@@ -16,51 +16,79 @@ renderHs path = do
   preprocessed <- Evaluate.preprocessFile path
   case preprocessed of
     Left err -> print err
-    Right blocks ->
-      TL.putStrLn . TL.fromChunks . intersperse "\n" . blocksToHs 0 $ blocks
+    Right blocks -> T.putStrLn $ renderBlocks blocks
+
+renderBlocks :: [Syntax.Block] -> Text
+renderBlocks = renderStrict . layoutPretty defaultLayoutOptions . prettyBlocks
 
 --------------------------------------------------------------------------------
-blocksToHs :: Int -> [Syntax.Block] -> [Text]
-blocksToHs indent = concatMap $ blockToHs indent
+prettyBlocks :: [Syntax.Block] -> Doc ann
+prettyBlocks = vsep . map prettyBlock
 
--- | Same as 'blocksToHs' but output @mempty@ instead of nothing when the list
--- of blocks is empty.
-blocksToHs' :: Int -> [Syntax.Block] -> [Text]
-blocksToHs' indent [] = [indent' <> "mempty"]
+prettyBlock :: Syntax.Block -> Doc ann
+prettyBlock (Syntax.PugElem name _ attrs children) = prettyPugElem (name, attrs', children)
  where
-  indent' = T.replicate (indent * 2) " "
-blocksToHs' indent blocks = blocksToHs indent blocks
+  attrs' = elemId <> elemClass
 
-blockToHs :: Int -> Syntax.Block -> [Text]
-blockToHs indent = \case
-  Syntax.BlockDoctype -> [indent' <> "H.docType"]
-  Syntax.PugElem name _ attrs children ->
-    el
-      <> elemId
-      <> elemClass
-      <> [indent' <> "  $"]
-      <> blocksToHs' (indent + 1) children
-   where
-    el = [indent' <> elemToHs name]
-    idNames = Syntax.idNamesFromAttrs attrs
-    idNames' :: Text
-    idNames' = T.intercalate " " idNames
-    elemId =
-      if idNames == []
-      then []
-      else [indent' <> "  ! A.id (H.toValue \"" <> idNames' <> "\")"]
-    classNames = Syntax.classNamesFromAttrs attrs
-    classNames' :: Text
-    classNames' = T.intercalate " " classNames
-    elemClass =
-      if classNames == []
-      then []
-      else [indent' <> "  ! A.class_ (H.toValue \"" <> classNames' <> "\")"]
- where
-  indent' = T.replicate (indent * 2) " "
+  idNames = Syntax.idNamesFromAttrs attrs
+  idNames' :: Text
+  idNames' = T.intercalate " " idNames
+  elemId =
+    if idNames == []
+    then []
+    else [Id' idNames']
 
-elemToHs :: Syntax.Elem -> Text
-elemToHs = \case
+  classNames = Syntax.classNamesFromAttrs attrs
+  classNames' :: Text
+  classNames' = T.intercalate " " classNames
+  elemClass =
+    if classNames == []
+    then []
+    else [Class' classNames']
+
+-- | Render an element, aligning the @!@ character:
+--
+-- @
+--   elem ! a
+--        ! b $ do
+--     child0
+--     child1
+-- @
+prettyPugElem :: (Syntax.Elem, [Attr'], [Syntax.Block]) -> Doc ann
+prettyPugElem (t1, [], as) =
+  let dollar = if length as > 1 then "$ do" else "$"
+      header = prettyElem t1 <+> dollar
+      footer = case as of
+        [] -> indent 2 "mempty"
+        _ -> vsep $ map (indent 2 . prettyBlock) as
+  in vsep [header, footer]
+prettyPugElem (t1, [t], as) =
+  let dollar = if length as > 1 then "$ do" else "$"
+      header = prettyElem t1 <+> "!" <+> prettyAttr t <+> dollar
+      footer = case as of
+        [] -> indent 2 "mempty"
+        _ -> vsep $ map (indent 2 . prettyBlock) as
+  in vsep [header, footer]
+prettyPugElem (t1, t:ts, as) =
+  let e = prettyElem t1
+      n = succ . T.length . renderStrict $ layoutPretty defaultLayoutOptions e
+      dollar = if length as > 1 then "$ do" else "$"
+      header = e <+> "!" <+> prettyAttr t
+      body = indent n $ vsep (map (("!" <+>) . prettyAttr) ts) <+> dollar
+      footer = case as of
+        [] -> indent 2 "mempty"
+        _ -> vsep $ map (indent 2 . prettyBlock) as
+  in vsep [header, body, footer]
+
+prettyElem :: Syntax.Elem -> Doc ann
+prettyElem = \case
   Syntax.Html -> "H.html"
   Syntax.Body -> "H.body"
   Syntax.Div -> "H.div"
+
+-- A version of Attr where multiple classes are supposed to be grouped together.
+data Attr' = Id' Text | Class' Text
+
+prettyAttr :: Attr' -> Doc ann
+prettyAttr (Id' t) = pretty $ "A.id (H.toValue \"" <> t <> "\")"
+prettyAttr (Class' t) = pretty $ "A.class_ (H.toValue \"" <> t <> "\")"
